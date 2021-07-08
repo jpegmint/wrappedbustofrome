b37b4a3c467184a4ae2152fa509a622144420acf
JPΞG <62081548+jpegdigital@users.noreply.github.com>
2021-07-08 18:53:21 -0500

    Added test cases.

--- test/WrappedBustOfRomeOneYear.test.js
+++ test/WrappedBustOfRomeOneYear.test.js
@@ -2,9 +2,19 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+/**
+ * @todo test tokenURI for accuracy.
+ * @todo test arweave assets match main assets.
+ * @todo test wrap function.
+ * @test test wrap function MINTER_ROLE.
+ * @todo test unwrap function
+ * @todo test unwrap function from various.
+ * @todo test wrap via safeTransferFrom.
+ */
 describe('wROME', function () {
 
     before(async function () {
+        [this.owner, this.addr1] = await ethers.getSigners();
         this.NiftyBuilderMaster = await ethers.getContractFactory("NiftyBuilderMaster");
         this.NiftyRegistry = await ethers.getContractFactory("NiftyRegistry");
         this.DateTime = await ethers.getContractFactory("DateTime");
@@ -13,29 +23,50 @@ describe('wROME', function () {
     });
 
     beforeEach(async function () {
-        const [owner, addr1] = await ethers.getSigners();
         this.masterBuilder = await this.NiftyBuilderMaster.deploy();
         await this.masterBuilder.deployed();
-        this.niftyRegistry = await this.NiftyRegistry.deploy([owner.address], []);
+        this.niftyRegistry = await this.NiftyRegistry.deploy([this.owner.address], []);
         await this.niftyRegistry.deployed();
-        await this.niftyRegistry.addNiftyKey(owner.address);
+        await this.niftyRegistry.addNiftyKey(this.owner.address);
         this.datetime = await this.DateTime.deploy();
         await this.datetime.deployed();
         this.nifty = await this.Nifty.deploy(this.datetime.address, this.masterBuilder.address, this.niftyRegistry.address);
         await this.nifty.deployed();
-
         await this.nifty.setNumNiftyPermitted(100);
-        await this.nifty.giftNifty(owner.address);
-        await this.nifty.giftNifty(owner.address);
-        await this.nifty.giftNifty(owner.address);
 
         this.wRome = await this.wROME.deploy(this.nifty.address, this.datetime.address);
         await this.wRome.deployed();
     });
+    
+    it('cannot wrap before approval', async function () {
+        await this.nifty.giftNifty(this.owner.address);
+        var tokenId = await this.nifty.tokenOfOwnerByIndex(this.owner.address, 0);
+        await expect(this.wRome.wrap(tokenId)).to.be.revertedWith('ERC721: transfer caller is not owner nor approved');
+    });
 
-    // Test case
-    it('get month returns current month', async function () {
-        const [owner, addr1] = await ethers.getSigners();
-        expect(1).to.equal(1);
+    it('can recover if received via transferFrom', async function () {
+        // Mint nifty and confirm owner owns token.
+        await this.nifty.giftNifty(this.owner.address);
+        var tokenId = await this.nifty.tokenOfOwnerByIndex(this.owner.address, 0);
+        expect(await this.nifty.ownerOf(tokenId)).to.equal(this.owner.address);
+
+        // Transfer to wRome and confirm it is owned by wRome.
+        await this.nifty.transferFrom(this.owner.address, this.wRome.address, tokenId);
+        expect(await this.nifty.ownerOf(tokenId)).to.equal(this.wRome.address);
+
+        // Check that no wrapped token was minted.
+        expect(await this.wRome.totalSupply()).to.equal(0);
+        await expect(this.wRome.ownerOf(tokenId)).to.be.revertedWith('ERC721: owner query for nonexistent token');
+
+        // Recover the token
+        await this.wRome.recoverOrphanedToken(tokenId);
+        expect(await this.nifty.ownerOf(tokenId)).to.equal(this.owner.address);
+    });
+
+    it('only admin can recover orphaned tokens', async function () {
+        await this.nifty.giftNifty(this.owner.address);
+        var tokenId = await this.nifty.tokenOfOwnerByIndex(this.owner.address, 0);
+        await this.nifty.transferFrom(this.owner.address, this.wRome.address, tokenId);
+        await expect(this.wRome.connect(this.addr1).recoverOrphanedToken(tokenId)).to.be.reverted;
     });
 });
