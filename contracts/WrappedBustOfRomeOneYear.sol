87bb0f098c06f774e8edc49734e2dd33e9331b52
JPΞG <62081548+jpegdigital@users.noreply.github.com>
2021-07-16 21:00:45 -0500

    Gas optimizations.

--- contracts/WrappedBustOfRomeOneYear.sol
+++ contracts/WrappedBustOfRomeOneYear.sol
@@ -23,23 +23,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract WrappedBustOfRomeOneYear is ERC721Wrapper, Ownable {
     using Strings for uint256;
 
+    /// The Nifty Gateway ROME contract
     INiftyBuilder private immutable _niftyBuilderInstance;
-    mapping(string => string) private _ipfsToArweaveIndex;
 
+    /**
+     * @dev Initialize and store Nifty Builder instance.
+     */
     constructor(address niftyBuilderAddress) ERC721("Wrapped Bust of Rome (One Year)", "wROME") {
         _niftyBuilderInstance = INiftyBuilder(niftyBuilderAddress);
-        _ipfsToArweaveIndex["QmQdb77jfHZSwk8dGpN3mqx8q4N7EUNytiAgEkXrMPbMVw"] = "iOKh8ppTX5831s9ip169PfcqZ265rlz_kH-oyDXELtA"; //State 1
-        _ipfsToArweaveIndex["QmS3kaQnxb28vcXQg35PrGarJKkSysttZdNLdZp3JquttQ"] = "4iJ3Igr90bfEkBMeQv1t2S4ctK2X-I18hnbal2YFfWI"; //State 2
-        _ipfsToArweaveIndex["QmX8beRtZAsed6naFWqddKejV33NoXotqZoGTuDaV5SHqN"] = "y4yuf5VvfAYOl3Rm5DTsAaneJDXwFJGBThI6VG3b7co"; //State 3
-        _ipfsToArweaveIndex["QmQvsAMYzJm8kGQ7YNF5ziWUb6hr7vqdmkrn1qEPDykYi4"] = "29SOcovLFC5Q4B-YJzgisGgRXllDHoN_l5c8Tan3jHs"; //State 4
-        _ipfsToArweaveIndex["QmZwHt9ZhCgVMqpcFDhwKSA3higVYQXzyaPqh2BPjjXJXU"] = "d8mJGLKJhg1Gl2OW1qQjcH8Y8tYBCvNWUuGH6iXd18U"; //State 5
-        _ipfsToArweaveIndex["Qmd2MNfgzPYXGMS1ZgdsiWuAkriRRx15pfRXU7ZVK22jce"] = "siy0OInjmvElSk2ORJ4VNiQC1_dkdKzNRpmkOBBy2hA"; //State 6
-        _ipfsToArweaveIndex["QmWcYzNdUYbMzrM7bGgTZXVE4GBm7v4dQneKb9fxgjMdAX"] = "5euBxS7JvRrqb7fxh4wLjEW5abPswocAGTHjqlrkyBE"; //State 7
-        _ipfsToArweaveIndex["QmaXX7VuBY1dCeK78TTGEvYLTF76sf6fnzK7TJSni4PHxj"] = "7IK1u-DsuAj0nQzpwmQpo66dwpWx8PS9i-xv6aS6y6I"; //State 8
-        _ipfsToArweaveIndex["QmaqeJnzF2cAdfDrYRAw6VwzNn9dY9bKTyUuTHg1gUSQY7"] = "LWpLIs3-PUvV6WvXa-onc5QZ5FeYiEpiIwRfc_u64ss"; //State 9
-        _ipfsToArweaveIndex["QmSZquD6yGy5QvsJnygXUnWKrsKJvk942L8nzs6YZFKbxY"] = "vzLvsueGrzpVI_MZBogAw57Pi1OdynahcolZPpvhEQI"; //State 10
-        _ipfsToArweaveIndex["QmYtdrfPd3jAWWpjkd24NzLGqH5TDsHNvB8Qtqu6xnBcJF"] = "iEh79QQjaMjKd0I6d6eM8UYcFw-pj5_gBdGhTOTB54g"; //State 11
-        _ipfsToArweaveIndex["QmesagGNeyjDvJ2N5oc8ykBiwsiE7gdk9vnfjjAe3ipjx4"] = "b132CTM45LOEMwzOqxnPqtDqlPPwcaQ0ztQ5OWhBnvQ"; //State 12
     }
 
     /**
@@ -58,9 +49,8 @@ contract WrappedBustOfRomeOneYear is ERC721Wrapper, Ownable {
         require(_exists(tokenId), "wROME: URI query for nonexistent token");
 
         string memory mintNumber = (tokenId - 100010000).toString();
-        string memory tokenHash = _niftyBuilderInstance.tokenIPFSHash(tokenId);
-        string memory imageUri = string(abi.encodePacked('https://arweave.net/', _ipfsToArweaveIndex[tokenHash]));
-        string memory animationUri = string(abi.encodePacked('ipfs://', tokenHash));
+        string memory ipfsHash = _niftyBuilderInstance.tokenIPFSHash(tokenId);
+        string memory arweaveHash = _getTokenArweaveHash(ipfsHash);
 
         bytes memory byteString;
         byteString = abi.encodePacked(byteString, 'data:application/json;utf8,{');
@@ -73,11 +63,35 @@ contract WrappedBustOfRomeOneYear is ERC721Wrapper, Ownable {
             ,'His piece will erode, reform, and change based on the time of year.",'
         );
         byteString = abi.encodePacked(byteString, '"external_url": "https://niftygateway.com/collections/danielarsham",');
-        byteString = abi.encodePacked(byteString, '"image": "', imageUri, '",', '"image_url": "', imageUri, '",');
-        byteString = abi.encodePacked(byteString, '"animation": "', animationUri, '",', '"animation_url": "', animationUri, '",');
+        byteString = abi.encodePacked(byteString
+            ,'"image": "https://arweave.net/', arweaveHash, '",'
+            ,'"image_url": "https://arweave.net/', arweaveHash, '",'
+            ,'"animation": "ipfs://', ipfsHash, '",'
+            ,'"animation_url": "ipfs://', ipfsHash, '",'
+        );
         byteString = abi.encodePacked(byteString, '"attributes":[{"trait_type": "Edition", "display_type": "number", "value": ', mintNumber, ', "max_value": 671}]');
         byteString = abi.encodePacked(byteString, '}');
         return string(byteString);
+    }
+
+    /**
+     * @dev Returns Arweave hash for the preview image matching given IPFS hash.
+     */
+    function _getTokenArweaveHash(string memory ipfsHash) private pure returns (string memory) {
+        bytes32 ipfsMatcher = keccak256(abi.encodePacked(ipfsHash));
+             if (ipfsMatcher == keccak256("QmQdb77jfHZSwk8dGpN3mqx8q4N7EUNytiAgEkXrMPbMVw")) return "iOKh8ppTX5831s9ip169PfcqZ265rlz_kH-oyDXELtA"; //State 1
+        else if (ipfsMatcher == keccak256("QmS3kaQnxb28vcXQg35PrGarJKkSysttZdNLdZp3JquttQ")) return "4iJ3Igr90bfEkBMeQv1t2S4ctK2X-I18hnbal2YFfWI"; //State 2
+        else if (ipfsMatcher == keccak256("QmX8beRtZAsed6naFWqddKejV33NoXotqZoGTuDaV5SHqN")) return "y4yuf5VvfAYOl3Rm5DTsAaneJDXwFJGBThI6VG3b7co"; //State 3
+        else if (ipfsMatcher == keccak256("QmQvsAMYzJm8kGQ7YNF5ziWUb6hr7vqdmkrn1qEPDykYi4")) return "29SOcovLFC5Q4B-YJzgisGgRXllDHoN_l5c8Tan3jHs"; //State 4
+        else if (ipfsMatcher == keccak256("QmZwHt9ZhCgVMqpcFDhwKSA3higVYQXzyaPqh2BPjjXJXU")) return "d8mJGLKJhg1Gl2OW1qQjcH8Y8tYBCvNWUuGH6iXd18U"; //State 5
+        else if (ipfsMatcher == keccak256("Qmd2MNfgzPYXGMS1ZgdsiWuAkriRRx15pfRXU7ZVK22jce")) return "siy0OInjmvElSk2ORJ4VNiQC1_dkdKzNRpmkOBBy2hA"; //State 6
+        else if (ipfsMatcher == keccak256("QmWcYzNdUYbMzrM7bGgTZXVE4GBm7v4dQneKb9fxgjMdAX")) return "5euBxS7JvRrqb7fxh4wLjEW5abPswocAGTHjqlrkyBE"; //State 7
+        else if (ipfsMatcher == keccak256("QmaXX7VuBY1dCeK78TTGEvYLTF76sf6fnzK7TJSni4PHxj")) return "7IK1u-DsuAj0nQzpwmQpo66dwpWx8PS9i-xv6aS6y6I"; //State 8
+        else if (ipfsMatcher == keccak256("QmaqeJnzF2cAdfDrYRAw6VwzNn9dY9bKTyUuTHg1gUSQY7")) return "LWpLIs3-PUvV6WvXa-onc5QZ5FeYiEpiIwRfc_u64ss"; //State 9
+        else if (ipfsMatcher == keccak256("QmSZquD6yGy5QvsJnygXUnWKrsKJvk942L8nzs6YZFKbxY")) return "vzLvsueGrzpVI_MZBogAw57Pi1OdynahcolZPpvhEQI"; //State 10
+        else if (ipfsMatcher == keccak256("QmYtdrfPd3jAWWpjkd24NzLGqH5TDsHNvB8Qtqu6xnBcJF")) return "iEh79QQjaMjKd0I6d6eM8UYcFw-pj5_gBdGhTOTB54g"; //State 11
+        else if (ipfsMatcher == keccak256("QmesagGNeyjDvJ2N5oc8ykBiwsiE7gdk9vnfjjAe3ipjx4")) return "b132CTM45LOEMwzOqxnPqtDqlPPwcaQ0ztQ5OWhBnvQ"; //State 12
+        revert('wROME: Invalid IPFS hash');
     }
 
     /**
